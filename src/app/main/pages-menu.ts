3da1948d81633e7a6238ac365fdf58fd15f521ab
Farrah Vecino <farrah.vecino@myxlog.com>
2021-03-17 23:40:52 +0800

    initial commit

---
+++ src/app/main/pages-menu.ts
@@ -0,0 +1,19 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'WELCOME USER',
+    group: true,
+  },
+  {
+    title: 'Dashboard',
+    icon: 'layout-outline',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'Reports Management',
+    icon: 'home-outline',
+    link: '/pages/iot-dashboard',
+  },
+];
