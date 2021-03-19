3e8d064eb1a8f12591d411cc7f4dc67934ccd370
athan <jonathan.miranda@myxlog.com>
2021-03-20 00:45:15 +0800

    report add file

--- src/app/main/pages.module.ts
+++ src/app/main/pages.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule, NbSelectModule } from '@nebular/theme';
+import { NbButtonModule, NbMenuModule, NbSelectModule } from '@nebular/theme';
 import { NbCardModule, NbIconModule, NbInputModule, NbTreeGridModule } from '@nebular/theme';
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
@@ -8,6 +8,7 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { InventoryComponent } from './inventory/inventory.component';
 import { ReportComponent } from './report/report.component';
+import { AddReportComponent } from './report/add-report/add-report.component';
 @NgModule({
   imports: [
     PagesRoutingModule,
@@ -17,14 +18,16 @@ import { ReportComponent } from './report/report.component';
     NbCardModule,
     NbTreeGridModule,
     NbIconModule,
-    NbInputModule,
     Ng2SmartTableModule,
     NbSelectModule,
+    NbButtonModule,
+    NbInputModule,
   ],
   declarations: [
     PagesComponent,
     InventoryComponent,
     ReportComponent,
+    AddReportComponent,
   ],
 })
 
